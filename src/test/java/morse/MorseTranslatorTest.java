eebf5ccb77c9c0440c49b00aec9f5967b989c8f2
kristinason <kristinason.sw@gmail.com>
2026-08-17 10:29:04 +0200

    Update MorseTranslatorTest.java

--- src/test/java/morse/MorseTranslatorTest.java
+++ src/test/java/morse/MorseTranslatorTest.java
@@ -1,4 +1,36 @@
-package morse;
+import morse.MorseTranslator;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 
 public class MorseTranslatorTest {
+   
+
+    @Test 
+    void textToMorse_HEJ() {
+        MorseTranslator t = new MorseTranslator();
+        assertEquals(".... . .---", t.textToMorse("HEJ"));
+    }
+    
+    @Test
+    void textToMorse_ignoresSpaces() {
+        MorseTranslator t = new MorseTranslator();
+        assertEquals(".... . .-.. .-.. ---", t.textToMorse("H E L L O"));
+    }
+
+    @Test
+    void morseToText_HEJ() {
+        assertEquals("HEJ", t.morseToText(".... . .---"));
+    }
+
+    @Test
+    void textToMorse_invalidCharacter_throwsException() { 
+        MorseTranslator t = new MorseTranslator();
+        assertThrows(IllegalArgumentException.class, 
+
+                 
+                () -> t.textToMorse("HELLO!"));
+    }
 }
